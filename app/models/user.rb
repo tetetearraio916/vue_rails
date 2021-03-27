08dee9f2a79e491bc24c8569f45e0c1da3dba85e
tetetearraio916 <notfase1192555@gmail.com>
2021-03-27 21:32:19 +0900

    add userモデルの検証を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, length: {maximum: 30}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password_digest, presence: true, length: {maximum: 15}
 end
