9be1c6d4ea2b6790b19f9bb5d47abdd9320e4c65
tetetearraio916 <notfase1192555@gmail.com>
2021-03-27 22:48:31 +0900

    add 暗号化・複号化の実装

---
+++ app/controllers/concerns/jwt_authenticator.rb
@@ -0,0 +1,19 @@
+module JwtAuthenticator
+  require 'jwt'
+
+  SECRET_KEY = Rails.application.secrets.secret_key_base
+
+  #暗号化
+  def encode(user_id)
+    #再ログインの期間
+    expires_in = 1.months.from_now.to_i
+    preload = { user_id: user_id, exp: expires_in }
+    JWT.encode(preload, SECRET_KEY_BASE, 'HS256')
+  end
+
+  #複合化
+  def decode(encoded_token)
+    decoded_dwt = JWT.decode(encoded_token, SECRET_KEY_BASE, true, algorithm: 'HS256')
+    decoded_dwt.first
+  end
+end
