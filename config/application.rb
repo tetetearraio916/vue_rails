5a13c228d38ad6c23ef94fc14ed1f6a4f2301fdc
tetetearraio916 <notfase1192555@gmail.com>
2021-03-27 21:21:37 +0900

    change ジェネレータの設定変更

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module VueRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
