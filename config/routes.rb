1731d36fe7e4f81702bd11f69eabd441d0433cd9
tetetearraio916 <notfase1192555@gmail.com>
2021-03-28 19:48:20 +0900

    fix sessionのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, only: %i[create]
-    resources :sessions, only: %i[create]
+    resource :session, only: %i[create]
   end
 end
