019f3bca20478a6219960bc1a55ee01def9c336b
tetetearraio916 <notfase1192555@gmail.com>
2021-03-28 16:28:58 +0900

    fix パスワードの文字の長さのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password_digest, presence: true, length: {maximum: 15}
+  validates :password_digest, presence: true
 end
