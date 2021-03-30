14596daa2db0d655796474c709c99b399a0b32dc
tetetearraio916 <notfase1192555@gmail.com>
2021-03-30 10:09:21 +0900

    fix sessionコントローラの修正

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,9 +6,9 @@ class Api::SessionsController < ApplicationController
     if @current_user&.authenticate(session_params[:password])
       jwt_token = encode(@current_user.id)
       response.headers['X-authenticate-token'] = jwt_token
-      render json: @current_user, each_serializer: UserSerializer, token: jwt_token
+      render json: {user: {id: @current_user.id, name: @current_user.name, email: @current_user.email, token: jwt_token}}
     else
-      render json: { error: {messages: ['メールアドレスまたはパスワードに誤りがあります']}}
+      render json: { error: {messages: ['メールアドレスまたはパスワードに誤りがあります']}}, status: 401
     end
   end
 
