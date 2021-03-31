a9439fa0ba28fb1f32f370c775f7ac4a7707f603
tetetearraio916 <notfase1192555@gmail.com>
2021-03-31 15:48:22 +0900

    fix ダイソンさんの解答例を元にシリアライザーの修正

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :token
-
-  def token
-    instance_options[:token] if instance_options[:token]
-  end
-
+  attributes :id, :name, :email
 end
